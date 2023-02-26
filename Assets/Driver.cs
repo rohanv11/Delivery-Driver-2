21b1beeab8606003f6460b375e0df4ff3808cbfa
rohanv11 <rohanvaradkar1110@gmail.com>
2023-02-27 00:43:21 +0530

    start with rotating gameobject

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class temp : MonoBehaviour
+public class Driver : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class temp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, 0, 0.1f);
     }
 }
